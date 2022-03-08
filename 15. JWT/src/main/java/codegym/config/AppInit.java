ba1d89a7ecb098cec8c92fe9c8c733bd02861431
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-03-08 22:22:26 +0700

    thuc_hanh_1

---
+++ 15. JWT/src/main/java/codegym/config/AppInit.java
@@ -0,0 +1,21 @@
+package codegym.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import codegym.security.SecurityConfig;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
